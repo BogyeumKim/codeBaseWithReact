e30f2c2b6097390d753b756c0a1b4f77e568d9b7
BogyeumKim <namastae112@gmail.com>
2025-07-08 22:31:21 +0900

    section5 - 35 파일 엔티티처리

---
+++ backend/mallapi/src/test/java/org/zerock/mallapi/repository/ProductRepositoryTests.java
@@ -0,0 +1,30 @@
+package org.zerock.mallapi.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.mallapi.domain.Product;
+import org.zerock.mallapi.repository.search.ProductRepository;
+
+import java.util.UUID;
+
+@SpringBootTest
+@Log4j2
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    public void testInsert() {
+
+        Product product = Product.builder().pname("TestName").pdesc("Test Desc").price(1000).build();
+
+        product.addImageString(UUID.randomUUID()+"_"+"IMAGE1.jpg");
+        product.addImageString(UUID.randomUUID()+"_"+"IMAGE2.jpg");
+
+        productRepository.save(product);
+
+    }
+}
