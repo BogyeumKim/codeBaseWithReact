fd950b38f704cb6d9e877c37abd19b5cb8f9757e
BogyeumKim <namastae112@gmail.com>
2025-07-14 23:23:13 +0900

    section6 - 45 Client 상품등록 처리

---
+++ front/ch1/src/pages/products/addPage.tsx
@@ -0,0 +1,12 @@
+import AddCompoenet from "../../components/products/addCoponent";
+
+function AddPage() {
+  return (
+    <div className="p-4 w-full bg-white">
+      <div className="text-3xl font-extrabold">Products Add Page</div>
+      <AddCompoenet />
+    </div>
+  );
+}
+
+export default AddPage;
--- front/ch1/src/router/productsRouter.tsx
+++ front/ch1/src/router/productsRouter.tsx
@@ -3,6 +3,7 @@ import { Navigate } from "react-router";
 
 const ProductsIndex = lazy(() => import("../pages/products/indexPage"));
 const ProductsList = lazy(() => import("../pages/products/listPage"));
+const ProductsAdd = lazy(() => import("../pages/products/addPage"));
 
 const Loading = () => <div>Products Loading....</div>;
 
@@ -22,6 +23,14 @@ export default function productsRouter() {
       {
         path: "",
         element: <Navigate to={"/products/list"}></Navigate>,
+      },
+      {
+        path: "add",
+        element: (
+          <Suspense fallback={<Loading />}>
+            <ProductsAdd />
+          </Suspense>
+        ),
       },
     ],
   };
