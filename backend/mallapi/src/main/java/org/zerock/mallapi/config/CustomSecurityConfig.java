ffc93ee7161c74582384997280a16e3aabb56c80
BogyeumKim <namastae112@gmail.com>
2025-07-17 23:33:01 +0900

    section7 - 54 로그인 성공 실패 처리

--- backend/mallapi/src/main/java/org/zerock/mallapi/config/CustomSecurityConfig.java
+++ backend/mallapi/src/main/java/org/zerock/mallapi/config/CustomSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,6 +14,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.zerock.mallapi.security.handler.APILoginFailHandler;
+import org.zerock.mallapi.security.handler.APILoginSuccessHandler;
 
 import java.util.Arrays;
 import java.util.List;
@@ -35,6 +38,12 @@ public class CustomSecurityConfig {
 
         http.formLogin(httpSecurityFormLoginConfigurer -> {
             httpSecurityFormLoginConfigurer.loginPage("/api/member/login");
+            httpSecurityFormLoginConfigurer.successHandler(new APILoginSuccessHandler());
+            httpSecurityFormLoginConfigurer.failureHandler(new APILoginFailHandler());
+        });
+
+        http.sessionManagement(config -> {
+            config.sessionCreationPolicy(SessionCreationPolicy.NEVER);
         });
 
         return http.build();
