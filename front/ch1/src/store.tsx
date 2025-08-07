f39bbc702e5d78798b45da8d99307e8e808120fa
BogyeumKim <namastae112@gmail.com>
2025-08-07 22:26:00 +0900

    section11 - 86 carSlice 작성

--- front/ch1/src/store.tsx
+++ front/ch1/src/store.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginSlice from "./slices/loginSlice";
+import cartSlice from "./slices/cartSlice";
 
 const store = configureStore({
   reducer: {
     loginSlice: loginSlice,
+    cartSlice: cartSlice,
   },
 });
 
